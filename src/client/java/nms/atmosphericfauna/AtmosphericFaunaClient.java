995f8af4df7ce69f5047ae7846e9c48b8eeb835c
RealNmS <sovicka.03@seznam.cz>
2026-01-01 16:54:18 +0100

    Imported fabric template mod files

---
+++ src/client/java/nms/atmosphericfauna/AtmosphericFaunaClient.java
@@ -0,0 +1,10 @@
+package nms.atmosphericfauna;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AtmosphericFaunaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
