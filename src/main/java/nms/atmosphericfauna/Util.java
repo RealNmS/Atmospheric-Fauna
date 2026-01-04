c1069b988498247d9cab7719dd118ccdde18ad28
RealNmS <sovicka.03@seznam.cz>
2026-01-04 11:18:21 +0100

    Added small fix to vars

--- src/main/java/nms/atmosphericfauna/Util.java
+++ src/main/java/nms/atmosphericfauna/Util.java
@@ -1,16 +1,18 @@
 package nms.atmosphericfauna;
 
+import org.jspecify.annotations.NonNull;
+
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.data.AtlasIds;
 import net.minecraft.resources.Identifier;
 
 public class Util {
-    public static Identifier getId(String path) {
+    public static Identifier getId(@NonNull String path) {
         return Identifier.fromNamespaceAndPath(AtmosphericFauna.MOD_ID, path);
     }
 
-    public static TextureAtlasSprite getSprite(String path) {
+    public static TextureAtlasSprite getSprite(@NonNull String path) {
         return Minecraft.getInstance().getAtlasManager().getAtlasOrThrow(AtlasIds.PARTICLES).getSprite(getId(path));
     }
 }
