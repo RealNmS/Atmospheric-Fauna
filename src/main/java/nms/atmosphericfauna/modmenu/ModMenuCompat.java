2b2ef12b387bcad70f4225e91b42115e901b8220
RealNmS <sovicka.03@seznam.cz>
2026-01-05 20:51:16 +0100

    Added cloth config missing screen

--- src/main/java/nms/atmosphericfauna/modmenu/ModMenuCompat.java
+++ src/main/java/nms/atmosphericfauna/modmenu/ModMenuCompat.java
@@ -1,11 +1,59 @@
 package nms.atmosphericfauna.modmenu;
 
+import org.jspecify.annotations.NonNull;
+
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 
 public class ModMenuCompat implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return ModMenuConfig::createConfigScreen;
+        if (isClothConfigAvailable()) {
+            try {
+                Class.forName("nms.atmosphericfauna.modmenu.ModMenuConfig");
+                return ModMenuConfig::createConfigScreen;
+            } catch (Throwable t) {
+            }
+        }
+
+        // Fallback screen
+        return parent -> new Screen(Component.translatable("title.atmosphericfauna.config")) {
+            @Override
+            protected void init() {
+                super.init();
+                Button info = Button.builder(Component.translatable("text.atmosphericfauna.cloth_missing"), b -> {
+                })
+                        .bounds(this.width / 2 - 170, this.height / 2 - 30, 340, 20)
+                        .build();
+                info.active = false;
+                this.addRenderableWidget(info);
+
+                this.addRenderableWidget(
+                        Button.builder(Component.translatable("gui.done"), b -> this.minecraft.setScreen(parent))
+                                .bounds(this.width / 2 - 100, this.height / 2 + 20, 200, 20)
+                                .build());
+            }
+
+            @Override
+            public void render(@NonNull GuiGraphics gui, int mouseX, int mouseY, float delta) {
+                gui.drawCenteredString(this.font,
+                        Component.translatable("text.atmosphericfauna.cloth_missing").getString(), this.width / 2,
+                        this.height / 2 - 6, 0xFF5555);
+                super.render(gui, mouseX, mouseY, delta);
+            }
+        };
+    }
+
+    private static boolean isClothConfigAvailable() {
+        try {
+            Class.forName("me.shedaniel.clothconfig2.api.ConfigBuilder");
+            return true;
+        } catch (Throwable t) {
+            return false;
+        }
     }
 }
