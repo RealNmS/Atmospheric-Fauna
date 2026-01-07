381dbe8f2230ef95ab6d31cb6ab78b9adeb8f172
RealNmS <sovicka.03@seznam.cz>
2026-01-07 22:06:16 +0100

    Turned the mod client-side only

--- src/main/java/nms/atmosphericfauna/AtmosphericFauna.java
+++ src/main/java/nms/atmosphericfauna/AtmosphericFauna.java
@@ -1,9 +1,11 @@
 package nms.atmosphericfauna;
 
 import nms.atmosphericfauna.config.ConfigHandler;
+import nms.atmosphericfauna.particle.CrowParticle;
 import nms.atmosphericfauna.spawning.AmbientSpawning;
-
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 import net.minecraft.core.Registry;
@@ -13,17 +15,31 @@ import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class AtmosphericFauna implements ModInitializer {
+public class AtmosphericFauna implements /* ModInitializer, */ ClientModInitializer {
 	public static final String MOD_ID = "atmospheric-fauna";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	public static final SimpleParticleType CROW = FabricParticleTypes.simple();
 
+	/*
+	 * only for now because idk how to set up client and server initializers
+	 * properly
+	 * 
+	 * @Override
+	 * public void onInitialize() {
+	 * LOGGER.info("Atmospheric Fauna is taking flight!");
+	 * ConfigHandler.load();
+	 * Registry.register(BuiltInRegistries.PARTICLE_TYPE,
+	 * Identifier.fromNamespaceAndPath(MOD_ID, "crow"), CROW);
+	 * ServerTickEvents.END_WORLD_TICK.register(AmbientSpawning::tick);
+	 * }
+	 */
+
 	@Override
-	public void onInitialize() {
-		LOGGER.info("Atmospheric Fauna is taking flight!");
+	public void onInitializeClient() {
+		LOGGER.info("[Atmospheric Fauna] Client is initializing...");
 		ConfigHandler.load();
 		Registry.register(BuiltInRegistries.PARTICLE_TYPE, Identifier.fromNamespaceAndPath(MOD_ID, "crow"), CROW);
-		ServerTickEvents.END_WORLD_TICK.register(AmbientSpawning::tick);
+		ParticleFactoryRegistry.getInstance().register(AtmosphericFauna.CROW, CrowParticle.Factory::new);
 	}
 }
