47caf4e3a1f05877d60d12da48d497a5d2f86a95
RealNmS <sovicka.03@seznam.cz>
2026-07-18 19:00:54 +0200

    Removed unused import

--- src/main/java/nms/atmosphericfauna/particle/BaseBirdParticle.java
+++ src/main/java/nms/atmosphericfauna/particle/BaseBirdParticle.java
@@ -14,7 +14,6 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.Vec3;
 
 public abstract class BaseBirdParticle extends BaseParticle {
 
