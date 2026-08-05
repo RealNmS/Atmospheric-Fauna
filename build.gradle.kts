cc72988e6d4161f63787045c2eb4550c9506b3cf
RealNmS <sovicka.03@seznam.cz>
2026-08-05 21:09:24 +0200

    Added Dev Environment Testing Mods

--- build.gradle.kts
+++ build.gradle.kts
@@ -64,6 +64,12 @@ dependencies {
 
     // Provides JSpecify annotations to older MC versions during compilation
     compileOnly("org.jspecify:jspecify:1.0.0")
+
+    // Dev Environment Testing Mods
+    if (sc.current.parsed >= "26.1") {
+        modLocalRuntime("maven.modrinth:spark:1.10.173-fabric") 
+        modLocalRuntime("maven.modrinth:ok-zoomer:17.0.0-beta.5") 
+    }
 }
 
 loom {
