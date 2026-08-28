ae5588071970ccc8ddeb267538b88a2b8b408673
RealNmS <sovicka.03@seznam.cz>
2026-08-28 12:07:11 +0200

    Fixed crash for version 1.21

--- src/main/java/nms/atmosphericfauna/mixin/SoundEngineMixin.java
+++ src/main/java/nms/atmosphericfauna/mixin/SoundEngineMixin.java
@@ -8,13 +8,22 @@ import net.minecraft.sounds.SoundSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+//? if <=1.21.1 {
+//import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+//?} else {
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+//?}
 
 @Mixin(SoundEngine.class)
 public class SoundEngineMixin {
     
+    //? if <=1.21.1 {
+    // @Inject(method = "play", at = @At("RETURN"))
+    // private void atmosphericfauna$onSoundPlayed(SoundInstance sound, CallbackInfo ci) {
+    //?} else {
     @Inject(method = "play", at = @At("RETURN"))
     private void atmosphericfauna$onSoundPlayed(SoundInstance sound, CallbackInfoReturnable<?> cir) {
+    //?}
         if (sound == null) return;
         
         SoundSource source = sound.getSource();
